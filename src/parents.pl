f89dc50ef2652ec22f94a27a8fd8202a1878fbd2
lawsonvt <lawson@hemoshear.com>
2019-01-13 17:21:30 -0500

    First

---
+++ src/parents.pl
@@ -0,0 +1,14 @@
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, pat).
+parent(bob, ann).
+parent(pat, jim).
+
+female(pam).
+male(tom).
+male(bob).
+female(liz).
+female(pat).
+female(ann).
+male(jim).
