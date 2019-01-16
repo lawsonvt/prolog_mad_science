bade4372a5bfcdd61f6dd66b5d98e614e49d7c37
lawsonvt <lawson@hemoshear.com>
2019-01-15 20:36:49 -0500

    More list operations, this shit is hard

--- src/list_ops.pl
+++ src/list_ops.pl
@@ -34,3 +34,37 @@ del(X, [Y|Tail], [Y|Tail1]) :-  % this ... is confusing
 
 insert(X, List, BiggerList) :-		% inserting X into list to make bigger list
 	del(X, BiggerList, List).	% is equivalent to removing X from bigger list to make list
+
+
+% sublist through conc
+sublist(S, L) :-		% sublist is a part of list
+	conc(L1, L2, L),	% breakdown L into L1 and L2
+	conc(S, L3, L2).	% sublist plus some L3 equals L2
+
+% permutations of a list
+permutation([],[]).		% empty list permutes to empty list
+
+permutation([X|L], P) :- 	% breakdown list into head and tail
+	permutation(L, L1),	% L1 is a permutation of L (the tail of our original list)
+	insert(X, L1, P).	% then adding X to L1 gives the permutation P
+
+% alternate form using delete
+permutation2([],[]).
+
+permutation2(L, [X|P]) :- 	% L is a permutation of some X and P
+	del(X, L, L1),		% deleting X from L gives us L1
+	permutation2(L1, P).	% which is a permutation of P
+
+% Exercises from page 73
+
+evenlength([]).			% this one is a mindfuck
+				% and proves that functors do not need to be declared before use
+evenlength([First|Rest]) :-
+	oddlength(Rest).
+oddlength([First|Rest]) :-
+	evenlength(Rest).
+
+
+
+
+
